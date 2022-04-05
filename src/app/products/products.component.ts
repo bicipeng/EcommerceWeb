4aff2bee50d5d77ea3afe44e3dc457020f87f695
BiciPeng <bicipeng@gmail.com>
2022-04-05 18:10:40 -0400

    refactored some code

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,25 +8,25 @@ import { Product } from '../Models/Product';
 import { ShoppingCart } from '../Models/Shopping-Cart';
 import { ProductService } from '../Services/product.service';
 import { ShoppingCartService } from '../Services/shopping-cart.service';
-import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
+
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit, OnDestroy {
+export class ProductsComponent implements OnInit{
 
 products: Product[]=[];
 filterProducts: Product[]=[];
 category?:string;
-shoppingCart:any;
+shoppingCart$!:Observable<ShoppingCart>;
 // subscription! : Subscription
 
 
   constructor(
-    route:ActivatedRoute,
-    productService:ProductService, 
+    private route:ActivatedRoute,
+    private productService:ProductService, 
     private shoppingCartService: ShoppingCartService,
     private db:AngularFireDatabase
    ) {
@@ -50,50 +50,50 @@ shoppingCart:any;
     }))
      */
     
-    productService.getAll().snapshotChanges().pipe(switchMap(eles=>{
-  
-      
-      eles.forEach(ele => {
-        const val: any = ele.payload.val();
-
-        this.products.push({
-          key: <string>ele.payload.key, 
-          title: <string>val.title,
-          price: <string>val.price, 
-          category: <string>val.category,
-          imgUrl: <string>val.imgUrl
-        });
-      });
-
-      return route.queryParamMap;
-    }
-    )
-    ). subscribe(params=>{
-    
-        this.category=params.get('category') as string;
-        this.filterProducts = (this.category) ?
-         this.products?.filter(p=>p.category.toLowerCase()=== this.category)
-         : this.products
-  
-      });
-
-
-
-    
    }
 
   async ngOnInit() {
     
-     (await this.shoppingCartService.getCart())
-     .subscribe((sc:any)=>{
-       console.log("here is shopping cart from service ", sc)
-      return  this.shoppingCart=sc});
-     
-  }
+    //  (await this.shoppingCartService.getCart())
+    //  .subscribe((sc:any)=>{
+    //    console.log("here is shopping cart from service ", sc)
+    //   return  this.shoppingCart=sc});
+    this.shoppingCart$ = await this.shoppingCartService.getCart()
 
-  ngOnDestroy(): void {
-      // this.subscription!.unsubscribe();
+    this.populateProducts();
+
+    
   }
+ private applyFilter(){
+  this.filterProducts = (this.category) ?
+  this.products?.filter(p=>p.category.toLowerCase()=== this.category)
+  : this.products
+
+}
+private populateProducts(){
+  
+  this.productService.getAll().snapshotChanges().pipe(switchMap(eles=>{  
+    eles.forEach(ele => {
+      const val: any = ele.payload.val();
+      this.products.push({
+        key: <string>ele.payload.key, 
+        title: <string>val.title,
+        price: <string>val.price, 
+        category: <string>val.category,
+        imgUrl: <string>val.imgUrl
+      });
+    });
+
+    return this.route.queryParamMap;
+  }
+  )
+  ). subscribe(params=>{
+  
+      this.category=params.get('category') as string;
+      this.applyFilter()
+
+    });
+}
 //  async getCart(){
 //   let cartId = await this.shoppingCartService.getOrCreateCartId()
 //   console.log("here is the shoppingCart Id", cartId)
